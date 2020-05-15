e38c2723e282de92622bf103cafe386e41a85e82
rleongPd5 <rleong00@stuy.edu>
2020-05-14 23:24:37 -0400

    for some reason the first images don't have anything?

--- script.py
+++ script.py
@@ -4,56 +4,50 @@ from matrix import *
 from draw import *
 
 """======== first_pass( commands ) ==========
-
   Checks the commands array for any animation commands
   (frames, basename, vary)
-
   Should set num_frames and basename if the frames
   or basename commands are present
-
   If vary is found, but frames is not, the entire
   program should exit.
-
   If frames is found, but basename is not, set name
   to some default value, and print out a message
   with the name being used.
   ==================== """
 def first_pass( commands ):
-    framesFound = False
-    varyFound = False
-    name = 'anim'
+
+    name = ''
     num_frames = 1
+    varyCmd = False
+    #print("HIII")
 
-    for command in commands:
-        c = command['op']
-        args = command['args']
-        if c == 'frames':
-            num_frames = int(args[0])
-            framesFound = True
-        elif c == 'basename':
-            name = args[0]
-        elif c == 'vary':
-            varyFound = True
+    for cmd in commands:
+        if(cmd['op'] == 'frames'):
+            num_frames = int(cmd['args'][0])
+            #print(num_frames)
+        elif(cmd['op'] == 'basename'):
+            name = cmd['args'][0]
+            #print(name)
+        elif(cmd['op'] == 'vary'):
+            varyCmd = True
 
-    if(varyFound and not framesFound):
-        print("ERROR: Number of frames not provided!")
-        return
-    if(framesFound and name == 'anim'):
-        print("No name provided. Placeholder name: 'anim'")
+    if(name == ''):
+        name = 'anim'
+        print('No basename found. Setting basename to anim.')
+
+    if(varyCmd and num_frames == 1):
+        raise('ERROR: vary found but only one frame.')
     return (name, num_frames)
 
 """======== second_pass( commands ) ==========
-
   In order to set the knobs for animation, we need to keep
   a seaprate value for each knob for each frame. We can do
   this by using an array of dictionaries. Each array index
   will correspond to a frame (eg. knobs[0] would be the first
   frame, knobs[2] would be the 3rd frame and so on).
-
   Each index should contain a dictionary of knob values, each
   key will be a knob name, and each value will be the knob's
   value for that frame.
-
   Go through the command array, and when you find vary, go
   from knobs[0] to knobs[frames-1] and add (or modify) the
   dictionary corresponding to the given knob with the
@@ -61,18 +55,24 @@ def first_pass( commands ):
   ===================="""
 def second_pass( commands, num_frames ):
     frames = [ {} for i in range(num_frames) ]
-    for command in commands:
-        c = command['op']
-        if c == 'vary':
-            args = command['args']
-            knob = command['knob']
-            start = int(args[0])
-            end = int(args[1])
-            value0 = int(args[2])
-            value1 = int(args[3])
-            deltak = (value1 - value0)/(end - start)
-            for i in range(start, end + 1):
-                frames[i][knob] = value0 + ((i - start) * deltak)
+
+    for cmd in commands:
+        if(cmd['op'] == 'vary'):
+            start_frame = int(cmd['args'][0])
+            end_frame = int(cmd['args'][1])
+            knoblist0 = cmd['args'][2]
+            knoblist1 = cmd['args'][3]
+
+            deltaKnob = (knoblist1 - knoblist0)/(end_frame - start_frame)
+            knob = cmd['knob']
+            curKnob = knoblist0
+
+            frame = start_frame
+            while (frame <= end_frame):
+                frames[frame][knob] = curKnob
+                curKnob += deltaKnob
+                frame += 1
+
     return frames
 
 
@@ -110,12 +110,11 @@ def run(filename):
 
     (name, num_frames) = first_pass(commands)
     frames = second_pass(commands, num_frames)
-    curr = 0
-    while (curr < num_frames):
-        if num_frames > 1:
-            print("Generating frame #" + str(curr))
-        else:
-            print("Generating single image")
+    #print(frames)
+
+    cur_frame = 0
+    while(cur_frame < num_frames):
+        print('Making frame ' + str(cur_frame))
 
         tmp = new_matrix()
         ident( tmp )
@@ -130,7 +129,7 @@ def run(filename):
         coords1 = []
 
         for command in commands:
-            # print(command)
+            #print(command)
             c = command['op']
             args = command['args']
             knob_value = 1
@@ -170,8 +169,8 @@ def run(filename):
                 draw_lines(tmp, screen, zbuffer, color)
                 tmp = []
             elif c == 'move':
-                if (command['knob']):
-                    knob = frames[curr][command['knob']]
+                if(command['knob']):
+                    knob = frames[cur_frame][command['knob']]
                 else:
                     knob = 1
                 tmp = make_translate(args[0] * knob, args[1] * knob, args[2] * knob)
@@ -179,8 +178,9 @@ def run(filename):
                 stack[-1] = [x[:] for x in tmp]
                 tmp = []
             elif c == 'scale':
-                if (command['knob']):
-                    knob = frames[curr][command['knob']]
+                if(command['knob']):
+                    knob = frames[cur_frame][command['knob']]
+                    print(knob)
                 else:
                     knob = 1
                 tmp = make_scale(args[0] * knob, args[1] * knob, args[2] * knob)
@@ -188,8 +188,8 @@ def run(filename):
                 stack[-1] = [x[:] for x in tmp]
                 tmp = []
             elif c == 'rotate':
-                if (command['knob']):
-                    knob = frames[curr][command['knob']]
+                if(command['knob']):
+                    knob = frames[cur_frame][command['knob']]
                 else:
                     knob = 1
                 theta = args[1] * (math.pi/180) * knob
@@ -212,11 +212,11 @@ def run(filename):
                 save_extension(screen, args[0] + ".png")
             # end operation loop
 
-        curr += 1
+        cur_frame += 1
         if(num_frames < 100):
-            save_extension(screen, 'anim/' + name + '%02d'%curr)
+            save_extension(screen, 'anim/' + name + '%02d'%cur_frame)
         else:
-            save_extension(screen, 'anim/' + name + '%03d'%curr)
+            save_extension(screen, 'anim/' + name + '%03d'%cur_frame)
 
     if(num_frames > 1):
         make_animation(name)
